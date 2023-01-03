cd89670f37c031373324ed88a34c9252f366aef3
Beatrice Costanza Marrano <beatrice.marrano@gmail.com>
2023-01-03 16:23:23 +0100

    new code fixed

--- model.py
+++ model.py
@@ -1,37 +1,35 @@
-        
-import torch
-import torch.nn.functional as F
-from torch import nn
+#from torch import nn
 
+
+#class MyAwesomeModel(nn.Module):
+    #def __init__(self):
+        #super().__init__()
+
+        
+        
+from torch import nn
 
 class MyAwesomeModel(nn.Module):
     def __init__(self):
         super().__init__()
-        # Inputs to hidden layer linear transformation
-        self.hidden1 = nn.Linear(784, 512)
-        self.hidden2 = nn.Linear(512, 256)
-        self.hidden3 = nn.Linear(256, 128)
-        # Output layer, 10 units - one for each digit
-        self.output = nn.Linear(128, 10)
+        self.backbone = nn.Sequential(
+            nn.Conv2d(1, 64, 3), # [N, 64, 26]
+            nn.LeakyReLU(),
+            nn.Conv2d(64, 32, 3), # [N, 32, 24]
+            nn.LeakyReLU(),
+            nn.Conv2d(32, 16, 3), # [N, 16, 22]
+            nn.LeakyReLU(),
+            nn.Conv2d(16, 8, 3), # [N, 8, 20]
+            nn.LeakyReLU()
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(8 * 20 * 20, 128),
+            nn.Dropout(),
+            nn.Linear(128, 10)
+        )
         
     def forward(self, x):
-        # Hidden layer with sigmoid activation
-        x = F.sigmoid(self.hidden1(x))
-        x = F.sigmoid(self.hidden2(x))
-        x = F.sigmoid(self.hidden3(x))
-        # Output layer with softmax activation
-        x = F.softmax(self.output(x), dim=1)
-        
-        return x        
-        
-        
-        
-        
-        
-        
-        
-        
-
-
-
-
+        return self.classifier(self.backbone(x))
+    
