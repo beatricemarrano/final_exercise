c7718ea82c143cbee9f87ea5b5f30464a4456050
Beatrice Costanza Marrano <beatrice.marrano@gmail.com>
2023-01-06 16:14:06 +0100

    fixed

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,19 +3,40 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-
+import os
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
+
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
-
+    
+    
+    content_train= []
+    content_test= []
+    os.path.isdir("../data/raw")
+    
+    for i in range(5):
+        content_train.append(np.load(f"train_{i}.npz", allow_pickle=True))
+        data_train = torch.tensor(np.concatenate([c['images'] for c in content_train])).reshape(-1, 1, 28, 28)
+        targets_train = torch.tensor(np.concatenate([c['labels'] for c in content_train]))
+    
+    content_test = np.load("test.npz", allow_pickle=True)
+    data_test = torch.tensor(content_test['images']).reshape(-1, 1, 28, 28).Normalize
+    targets_test = torch.tensor(content_test['labels'])
+    
+    torch.save(data_train, os.path.join('../data/processed', "data_train.pkl"))
+    torch.save(data_test, os.path.join('../data/processed', "data_test.pkl"))
+    torch.save(targets_train, os.path.join('../data/processed', "targets_train.pkl"))
+    torch.save(targets_test, os.path.join('../data/processed', "targets_test.pkl"))
+    
+    
+    
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
@@ -28,64 +49,3 @@ if __name__ == '__main__':
     load_dotenv(find_dotenv())
 
     main()
-
-
-
-#import torch
-
-
-#def mnist():
-    # exchange with the corrupted mnist dataset
-    #train = torch.randn(50000, 784)
-    #test = torch.randn(10000, 784) 
-    #return train, test
-
-
-
-import torch
-import wget
-from torch.utils.data import Dataset
-import numpy as np
-import os
-
-class CorruptMnist(Dataset):
-    def __init__(self, train):
-        self.download_data(train)
-        if train:
-            content = [ ]
-            for i in range(5):
-                content.append(np.load(f"train_{i}.npz", allow_pickle=True))
-            data = torch.tensor(np.concatenate([c['images'] for c in content])).reshape(-1, 1, 28, 28)
-            targets = torch.tensor(np.concatenate([c['labels'] for c in content]))
-        else:
-            content = np.load("test.npz", allow_pickle=True)
-            data = torch.tensor(content['images']).reshape(-1, 1, 28, 28)
-            targets = torch.tensor(content['labels'])
-            
-        self.data = data
-        self.targets = targets
-    
-    def download_data(self, train):
-        files = os.listdir()
-        if train:
-            for file_idx in range(5):
-                if f'train_{file_idx}.npy' not in files:
-                    wget.download(f"https://raw.githubusercontent.com/SkafteNicki/dtu_mlops/main/data/corruptmnist/train_{file_idx}.npz")
-        else:
-            if "test.npy" not in files:    
-                wget.download("https://raw.githubusercontent.com/SkafteNicki/dtu_mlops/main/data/corruptmnist/test.npz")
-    
-    def __len__(self):
-        return self.targets.numel()
-    
-    def __getitem__(self, idx):
-        return self.data[idx].float(), self.targets[idx]
-
-
-if __name__ == "__main__":
-    dataset_train = CorruptMnist(train=True)
-    dataset_test = CorruptMnist(train=False)
-    print(dataset_train.data.shape)
-    print(dataset_train.targets.shape)
-    print(dataset_test.data.shape)
-    print(dataset_test.targets.shape)
