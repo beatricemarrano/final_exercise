966f45f2789a0ff097e32e6cdf6f6981d51f9f72
Beatrice Costanza Marrano <beatrice.marrano@gmail.com>
2023-01-10 14:55:22 +0100

    fix

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,6 +3,10 @@ import sys
 import os
 import torch
 import wandb
+#import logging
+#from pathlib import Path
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
 
 sys.path.append("/Users/mac/Documents/GitHub/final_exercise/src")
 from data.data import CorruptMnist
