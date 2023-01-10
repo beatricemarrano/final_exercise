f88b69e1814ce3fd7f7444511437d69ac4cdcb31
Beatrice Costanza Marrano <beatrice.marrano@gmail.com>
2023-01-10 17:50:56 +0100

    code fixed with lightning

--- src/models/train_model_lightning.py
+++ src/models/train_model_lightning.py
@@ -6,10 +6,11 @@ import hydra
 import pytorch_lightning as pl
 import torch
 import wandb
+import sys
 from dotenv import find_dotenv, load_dotenv
-from google.cloud import secretmanager
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import Callback #importing Callbacks class
 
 sys.path.append("/Users/mac/Documents/GitHub/final_exercise/src")
 from data.data import CorruptMnistDataModule
@@ -17,6 +18,8 @@ from data.data import CorruptMnistDataModule
 from models.model import MyAwesomeModel
 
 import matplotlib.pyplot as plt
+
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 @hydra.main(config_path="../../config", config_name="default_config.yaml")
 def main(config: DictConfig):
@@ -34,23 +37,28 @@ def main(config: DictConfig):
     wandb.init(project="final_exercise", entity="beatrice-marrano", config= config)
     
     data_module = CorruptMnistDataModule(
-        os.path.join(hydra.utils.get_original_cwd(), config.data.path),
+        os.path.join(hydra.utils.get_original_cwd(), config.data.path),###
         batch_size=config.train.batch_size,
     )
     data_module.setup()
     model = MyAwesomeModel(config)
 
+    checkpoint_callback = ModelCheckpoint(dirpath="./models", monitor="val_loss", mode="min")
+    
     trainer = Trainer(
+        default_root_dir=os.getcwd(),
         max_epochs=config.train.epochs,
-        logger=pl.loggers.WandbLogger(project="final_exercise", config=config),
-        val_check_interval=1.0,
+        logger=pl.loggers.WandbLogger(project="final_exercise", config=config),#Add the wandb logger
+        enable_checkpointing=True, #Callbacks ModelCheckpoint
+        callbacks=[checkpoint_callback],
+        limit_test_batches=0.25, # run through only 25% of the test set each epoch
         check_val_every_n_epoch=1,
         gradient_clip_val=1.0,
     )
     trainer.fit(
         model,
-        train_dataloader=data_module.train_dataloader(),
-        val_dataloaders=data_module.test_dataloader(),
+        train_dataloaders=data_module.train_dataloader(),
+        #val_dataloaders=data_module.test_dataloader(),
     )
 
     torch.save(model, os.path.join('/Users/mac/Documents/GitHub/final_exercise/models', "trained_model.pt"))    
