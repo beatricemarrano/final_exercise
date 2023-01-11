d28897336782005207683c5461142947f4ceaea0
Beatrice Costanza Marrano <beatrice.marrano@gmail.com>
2023-01-11 14:42:29 +0100

    fixed pytest

--- src/models/model.py
+++ src/models/model.py
@@ -28,6 +28,10 @@ class MyAwesomeModel(LightningModule):
         self.criterium= nn.CrossEntropyLoss()
         
     def forward(self, x):
+        if x.ndim != 4:
+            raise ValueError('Expected input to a 4D tensor')
+        if x.shape[1] != 1 or x.shape[2] != 28 or x.shape[3] != 28:
+            raise ValueError('Expected each sample to have shape [1, 28, 28]')
         return self.classifier(self.backbone(x))
     
     def training_step(self, batch, bathc_idx):
