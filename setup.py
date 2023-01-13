e7061361683a859be38307467db5325fe6f4c3e0
Beatrice Costanza Marrano <beatrice.marrano@gmail.com>
2023-01-13 13:34:42 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import find_packages, setup
+#from setuptools import find_packages, setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='src',
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     version='0.1.0',
     description='this is an example project to showoff how cookiecutter works.',
     author='Beatrice Marrano',
