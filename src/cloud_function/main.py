98a1c1c9c8498803cded7f9b15ae01d71ccf6b8d
Beatrice Costanza Marrano <beatrice.marrano@gmail.com>
2023-01-18 08:25:35 +0100

    fix

--- src/cloud_function/main.py
+++ src/cloud_function/main.py
@@ -1,4 +1,4 @@
-#import io
+import io
 import pickle
 #import nltk
 import torch
@@ -13,20 +13,29 @@ client = storage.Client()
 bucket = client.get_bucket(BUCKET_NAME)
 blob = bucket.get_blob(MODEL_FILE)
 my_model = blob.download_as_string()
-my_model = pickle.load(my_model)#
+my_model = io.BytesIO(my_model)
+
+model = torch.jit.load(my_model)
 
 
-
-def predict(request):
-   """ will to stuff to your request """
-   request_json = request.get_json()
-   if request_json and 'input_data' in request_json:
-         data = request_json['input_data']
-         input_data = list(map(int, data.split(',')))
-         prediction = my_model.predict([input_data])
-         return f'Belongs to class: {prediction}'
-   else:
-         return 'No input data received'
-
+def commands(request):
+    """if input= 0 print model, if input=1 print parameters"""
+    request_json = request.get_json()
+    if request_json['input']==0:
+        print("The model is:")
+        print(model)
+        print('=====')
+    elif request_json['input']==1:
+        print("The parameters are:")
+        for name, param in model.named_parameters():
+            print('name: ', name)
+            print(type(param))
+            print('param.shape: ', param.shape)
+            print('param.requires_grad: ', param.requires_grad)
+            print('=====')
+    else:
+        print("No choice of input request received")
+        print('=====')
 
 print("init done")
+print('=====')
